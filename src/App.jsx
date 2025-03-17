8fb0c71bef2d17953abe6ebe361e56dae45848f9
Matthew Rife <dxfan218@yahoo.com>
2025-03-17 19:00:48 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+function App() {
+  const generateToast = () => {
+    toast.success("A lovely toast!");
+  };
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold hover:text-blue-300">
+        React + Vite + Tailwind
+      </h1>
+      <Button
+        className="bg-blue-400 text-white hover:bg-blue-300 hover:animate-bounce m-4"
+        onClick={generateToast}
+      >
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
