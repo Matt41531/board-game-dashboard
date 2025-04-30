4183bf60a6774eb7acf1939933d8777e84375b0b
Matthew Rife <dxfan218@yahoo.com>
2025-04-29 20:46:27 -0400

    Home Page font color contrast

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="flex flex-col items-center justify-center h-screen w-full">
       <h1 className="text-3xl font-bold hover:text-chart-4">Wingspan Cards</h1>
       <Button
-        className="bg-chart-4 text-background hover:bg-chart-4/50 hover:animate-bounce m-4"
+        className="bg-chart-4 text-foreground hover:bg-chart-4/50 hover:animate-bounce m-4"
         onClick={navigateToCards}
       >
         Go To Cards
