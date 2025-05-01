c63e638cec66a17013c7ce40c4e350e508d922fe
Matthew Rife <dxfan218@yahoo.com>
2025-04-30 22:04:02 -0400

    Aspect ratio and width/height properties for cards to prevent layout shift

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -9,12 +9,15 @@ function Cards() {
       <main className="flex flex-wrap gap-4 justify-center">
         {cardResults &&
           cardResults.map((card) => (
-            <img
-              key={card.id}
-              src={card.img_url}
-              alt={card.name}
-              className="w-1/8 hover:scale-110 transition"
-            />
+            <div className="aspect-[240/336] w-[240px]" key={card.id}>
+              <img
+                src={card.img_url}
+                alt={card.name}
+                width={240}
+                height={336}
+                className="w-full h-full hover:scale-110 transition object-cover"
+              />
+            </div>
           ))}
       </main>
     </div>
