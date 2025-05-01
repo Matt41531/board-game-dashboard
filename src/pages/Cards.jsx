bf4d09d1554ab541a281e25ced59200f84621fe7
Matthew Rife <dxfan218@yahoo.com>
2025-05-01 09:14:06 -0400

    Switching cards to CSS grid to prevent layout shift

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -6,11 +6,14 @@ function Cards() {
   const { cardResults } = useContext(CardsContext);
   console.log(cardResults);
   return (
-    <div className="flex-1 overflow-auto p-4">
-      <main className="flex flex-wrap gap-4 justify-center">
+    <div className="flex-1 overflow-auto p-4 h-full w-full">
+      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 gap-1 min-h-0">
         {cardResults &&
           cardResults.map((card) => (
-            <div className="aspect-[240/336] w-[240px]" key={card.id}>
+            <div 
+              className="aspect-[240/336] w-[240px] mx-auto" 
+              key={card.id}
+            >
               <TransformedImage
                 src={card.img_url}
                 alt={card.name}
